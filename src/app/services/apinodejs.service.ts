fe7a87bb06a8197dcb02b3a477628630f13c899f
luis <luiscg404@gmail.com>
2021-03-04 18:57:17 -0600

    Se agrega NodeJS a docker

--- src/app/services/apinodejs.service.ts
+++ src/app/services/apinodejs.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 //const URLDEV = 'https://nekonokokoroapi.herokuapp.com/';
-const URLDEV = 'http://localhost:3000/';
+const URLDEV = 'http://192.168.1.65:3000/';
 
 @Injectable({
   providedIn: 'root'
