69514c0eac7102e886cb8ffab04f704941117ed5
luis <luiscg404@gmail.com>
2021-03-05 10:56:18 -0600

    Se agrega netcore en linux

--- src/app/services/apinetcore.service.ts
+++ src/app/services/apinetcore.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 //const URLDEV = 'https://apicoremtwdmproyecto.azurewebsites.net/api/';
-const URLDEV = 'https://localhost:5001/api/';
+const URLDEV = 'http://192.168.1.65:500/api/';
 
 @Injectable({
   providedIn: 'root'
